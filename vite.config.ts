f34c112627fb4ba1161a8300689bd44bc551c601
jpedro-cf <jpedroswc453@gmail.com>
2025-09-22 20:32:48 -0300

    chore: env vars

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/UniLib_Front/',
+    base: '/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
